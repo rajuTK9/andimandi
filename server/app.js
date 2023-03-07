648a13c5f470507ff9ac03576a7332a7895f8f98
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-03-08 01:39:40 +0530

    Backend created successfully

---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+// Setting up DotEnv
+dotenv.config({ path: "./.env" });
+// Establishing a connection to DB
+require("./db/conn");
+// For json inputs
+app.use(express.json());
+// Setting up routes
+app.use(require("./routes/auth"));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
