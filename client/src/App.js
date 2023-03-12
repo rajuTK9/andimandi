7a1fbfb3e6d9f1d2a0cd9f51b807db772662892c
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-03-12 19:13:30 +0530

    Background images added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import AboutPage from './components/AboutPage/AboutPage';
-import LandingPage from './components/LandingPage/LandingPage';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AboutPage from "./components/AboutPage/AboutPage";
+import LandingPage from "./components/LandingPage/LandingPage";
+import MainBhadaas from "./components/LandingPage/MainBhadaas";
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<LandingPage />} />
-      <Route path='/about' element={<AboutPage />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/bhadaas" element={<MainBhadaas />} />
     </Routes>
   );
 }
--- client/src/components/LandingPage/MainBhadaas.js
+++ client/src/components/LandingPage/MainBhadaas.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import './LandingPage.css'
-import LikeButton from '../Miscellanous/LikeButton';
+import React from "react";
+import "./LandingPage.css";
+import "./MainBhadaas.css";
+import LikeButton from "../Miscellanous/LikeButton";
 
 function MainBhadaas() {
   return (
-    <div className='mainbhadaas'>
-      <h1 className='bhadaas-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</h1>
-      <div className='flex'>
-        <LikeButton count={34} />
-        <p className='info'>*They paid us to display this</p>
+    <div className="mainbhadaas">
+      <div className="mainbhadaas-container">
+        <h1 className="bhadaas-text">Lorem Ipsum ki Maa ko Pranam! 🙄</h1>
+        <div className="flex bhadaas-body">
+          <LikeButton count={34} />
+          <p className="info bhadaas-info">*They paid us to display this</p>
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default MainBhadaas
+export default MainBhadaas;
